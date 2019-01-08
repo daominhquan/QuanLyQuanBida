a677312f22b91cb109a99a375ebab13f92243ca1
Đào Minh Quâb <quan.dm.15dh110176@outlook.com>
2019-01-08 09:32:09 +0700

    upload source code

---
+++ QuanBida/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace QuanBida
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnDangKy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbnhaplaimatkhau = new System.Windows.Forms.Label();
+            this.txtnhaplaimatkhau = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(168, 164);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(256, 20);
+            this.txtUserName.TabIndex = 0;
+            this.txtUserName.TextChanged += new System.EventHandler(this.txtnhaplaimatkhau_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(168, 201);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(256, 20);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtnhaplaimatkhau_TextChanged);
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Location = new System.Drawing.Point(168, 304);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(127, 51);
+            this.btnDangNhap.TabIndex = 2;
+            this.btnDangNhap.Text = "đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = true;
+            this.btnDangNhap.Click += new System.EventHandler(this.btnDangNhap_Click);
+            // 
+            // btnDangKy
+            // 
+            this.btnDangKy.Location = new System.Drawing.Point(297, 304);
+            this.btnDangKy.Name = "btnDangKy";
+            this.btnDangKy.Size = new System.Drawing.Size(127, 51);
+            this.btnDangKy.TabIndex = 3;
+            this.btnDangKy.Text = "đăng ký";
+            this.btnDangKy.UseVisualStyleBackColor = true;
+            this.btnDangKy.Click += new System.EventHandler(this.btnDangKy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(109, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "tài khoản";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "mật khẩu";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Sitka Text", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(193, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 53);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "đăng nhập";
+            // 
+            // lbnhaplaimatkhau
+            // 
+            this.lbnhaplaimatkhau.AutoSize = true;
+            this.lbnhaplaimatkhau.Location = new System.Drawing.Point(69, 240);
+            this.lbnhaplaimatkhau.Name = "lbnhaplaimatkhau";
+            this.lbnhaplaimatkhau.Size = new System.Drawing.Size(91, 13);
+            this.lbnhaplaimatkhau.TabIndex = 8;
+            this.lbnhaplaimatkhau.Text = "nhập lại mật khẩu";
+            // 
+            // txtnhaplaimatkhau
+            // 
+            this.txtnhaplaimatkhau.Location = new System.Drawing.Point(168, 237);
+            this.txtnhaplaimatkhau.Name = "txtnhaplaimatkhau";
+            this.txtnhaplaimatkhau.Size = new System.Drawing.Size(256, 20);
+            this.txtnhaplaimatkhau.TabIndex = 7;
+            this.txtnhaplaimatkhau.TextChanged += new System.EventHandler(this.txtnhaplaimatkhau_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 432);
+            this.Controls.Add(this.lbnhaplaimatkhau);
+            this.Controls.Add(this.txtnhaplaimatkhau);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDangKy);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnDangKy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbnhaplaimatkhau;
+        private System.Windows.Forms.TextBox txtnhaplaimatkhau;
+    }
+}
+
---
+++ QuanBida/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanBida
+{
+    
+    public partial class Form1 : Form
+    {
+        private CodeFirstDBEntities1 db = new CodeFirstDBEntities1();
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void btnDangKy_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text == txtnhaplaimatkhau.Text)
+            {
+                var acc = new Account();
+                acc.UserName = txtUserName.Text;
+                acc.Password = txtPassword.Text;
+                db.Accounts.Add(acc);
+                db.SaveChanges();
+                MessageBox.Show("Thành công");
+            }
+            else
+            {
+                MessageBox.Show("Không Khớp");
+            }
+            
+        }
+
+        private void txtnhaplaimatkhau_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            var listAccount = db.Accounts.ToList();
+            foreach (var item in listAccount)
+            {
+                if (item.UserName == txtUserName.Text && item.Password == txtPassword.Text)
+                {
+                    MessageBox.Show("Đăng nhập thành công");
+                    return;
+                }
+               
+            }
+            foreach (var item in listAccount)
+            {
+                if (item.UserName == txtUserName.Text && item.Password != txtPassword.Text)
+                {
+                    MessageBox.Show("Nhập sai mật khẩu của tài khoản này");
+                    return;
+
+                }
+            }
+            foreach (var item in listAccount)
+            {
+                if (item.UserName != txtUserName.Text)
+                {
+                    MessageBox.Show("tài khoản không tồn tại , vui lòng nhập tài khoản khác");
+                    return;
+                }
+            }
+        }
+    }
+}
